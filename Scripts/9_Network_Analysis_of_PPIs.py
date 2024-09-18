19be96291455339213409e99d78709ab6999d0db
Jonathan Josephs-Spaulding <49744158+PoeticPremium6@users.noreply.github.com>
2024-09-18 14:32:27 +0100

    Update 9_Network_Analysis_of_PPIs.py

--- Scripts/9_Network_Analysis_of_PPIs.py
+++ Scripts/9_Network_Analysis_of_PPIs.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-base_directory = 'C:\\Users\\jonat\\OneDrive - University of Glasgow\\Metalloproteome\\Submission\\'  # Adjust this path if necessary
+base_directory = '.../'  # Adjust this path if necessary
 
 # Load data
 def load_data(file_path):
